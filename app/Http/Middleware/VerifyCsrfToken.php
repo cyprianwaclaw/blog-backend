7558bec2eeb841eb819b3519d63c4f30368f6580
Cyprian Wacław <cyprianwaclaw@gmail.com>
2024-01-13 12:33:23 +0100

    🐛  Fix error: csrf token mismatch in api connect

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,12 +6,16 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
+	public function __construct()
+	{
+	}
     /**
      * The URIs that should be excluded from CSRF verification.
      *
      * @var array<int, string>
      */
+    //musi byc, " '/api/*'" bo przy polaczeniu ze strona pojawia sie blad tokenu crf
     protected $except = [
-        //
+        '/api/*'
     ];
 }
